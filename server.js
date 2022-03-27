638ead75eb4ff500de7bd9e34cc2bee6ce3cb3b4
JImmy <59650459+okesseko@users.noreply.github.com>
2022-03-27 19:38:34 +0800

    feat: cors setting

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const articleRouter = require("./routers/article");
 
 const url = "mongodb://localhost:27017/PersonalWeb";
+const corsOptions = {
+  origin: ["http://www.example.com", "http://localhost:8000"],
+};
 
 const app = express();
 
@@ -13,6 +17,7 @@ con.on("open", () => {
   console.log("connected...");
 });
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(articleRouter);
