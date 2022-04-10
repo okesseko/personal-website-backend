492b0ccb076b25372a934a8830afd5cb7a7876e3
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-10 22:43:04 +0800

    feat: add search condition & delete func & change id create method

--- routers/category.js
+++ routers/category.js
@@ -5,18 +5,29 @@ const router = express.Router();
 
 router.get("/category", async (req, res) => {
   try {
-    const filter = {};
-    if (req.query.value) filter.value = req.query.value;
-    const categories = await categoryModal.find(filter);
+    const { limit, page, order, ...filterCondition } = req.query;
 
-    res.status(200).send(categories);
+    const filter = filterCondition;
+
+    const categories = await categoryModal.find(filter, null, {
+      limit: limit,
+      skip: page - 1,
+      sort: { releaseTime: order },
+    });
+
+    const totalSize = await categoryModal.find(filter).count();
+
+    res.status(200).send({ categories, totalSize });
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
 router.post("/category", async (req, res) => {
-  const id = (await categoryModal.find({}).count()) + 1;
+  const id =
+    parseInt(
+      (await categoryModal.findOne().sort({ createTime: -1 }).id) || "0"
+    ) + 1;
   const category = new categoryModal({ ...req.body, id });
 
   try {
@@ -41,6 +52,18 @@ router.patch("/category/:id", async (req, res) => {
 
     const save = await category.save();
     return res.status(204).send(save);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete("/category/:id", async (req, res) => {
+  try {
+    if (!req.params.id) res.status(500).send("Id is necessary");
+
+    await categoryModal.deleteOne({ id: req.params.id });
+
+    return res.status(200).send("");
   } catch (err) {
     res.status(500).send(err);
   }
